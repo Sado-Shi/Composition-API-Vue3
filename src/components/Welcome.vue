5b6540477327dc065f5a753a75bb04fc8f04c145
Sado-Shi <derasado04@gmail.com>
2022-01-04 00:01:30 +0900

    [practice]ts 型

---
+++ src/components/Welcome.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="welcome">
+    <h1>{{ msg }}</h1>
+    <button @click="changeMsg('msg!')">Click!</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'Welcome',
+
+  data () {
+    return {
+      msg: 'Hello!TypeScript!'
+    }
+  },
+
+  computed: {
+    getLength (): number {
+      return this.msg.length
+    }
+  },
+
+  methods: {
+    changeMsg (msg: string) {
+      this.msg = msg
+    }
+  }
+
+  // setup () {
+  //   const msg = 'Hello!'
+
+  //   const getLength: string = computed(() => {
+  //     return msg
+  //   })
+
+  //   return { msg, getLength }
+  // }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
